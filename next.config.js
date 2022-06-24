4e0803bee31782fcd2804038c11f304140c13396
DanMarqz <danielarturo.marquez@gmail.com>
2022-06-24 10:21:41 -0400

    Continuar platzi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+/*  env: {
+    customKey: 'customValue ',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redorects() {
+    return [
+      {
+        source: '/hola',
+        destination: '/hello',
+        permanent: true,
+      }
+    ]
+  }*/
 }
 
 module.exports = nextConfig
---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext';
+import close from '@icons/icon_close.png';
+import styles from '@styles/OrderItem.module.scss';
+
+const OrderItem = ({ product, indexValue }) => {
+
+	const { removeFromCart } = useContext(AppContext);
+	
+	const handleRemove = index => {
+		removeFromCart(index);
+	}
+
+	return (
+		<div className={styles.OrderItem}>
+			<figure>
+				<img src={product.images[0]} alt={product.title} />
+			</figure>
+			<p>{product.title}</p>
+			<p>{product.price}</p>
+			<img src={close} alt="close" onClick={() => handleRemove(indexValue)} />
+		</div>
+	);
+}
+
+export default OrderItem;
